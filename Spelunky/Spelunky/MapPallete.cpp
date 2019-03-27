a690c2ab96ea31bfb2be8b33b04062a8504e86e4
Paper3300 <paperring3300@gmail.com>
2019-03-27 21:42:09 +0900

    ## 권순우 ## <타일맵툴 구현중>

--- Spelunky/Spelunky/MapPallete.cpp
+++ Spelunky/Spelunky/MapPallete.cpp
@@ -6,12 +6,13 @@
 #include "Transform.h"
 
 MapPallete::MapPallete()
-	:mCanvas(new Canvas((D2D1::ColorF::Enum)0x555599, 0.6f, Vector2(_WinSizeX - 300, _WinSizeY / 2 - 300), Vector2(300, 500), Pivot::LeftTop))
+	:mCanvas(new Canvas((D2D1::ColorF::Enum)0x555599, 0.6f, Vector2(_WinSizeX - 300, _WinSizeY / 2 - 300), Vector2(300, 500), Pivot::LeftTop)),
+	mIsPick(false)
 {
-
 	float size = 80.f;
 	float offsetX = (300.f - size) / 4.f / 4.f;
 	Vector2 canvasPos = mCanvas->GetTransform()->GetWorldPosition();
+	int index = 1;
 	for (UINT y = 0; y < 5; ++y)
 	{
 		for (UINT x = 0; x < 3; ++x)
@@ -21,16 +22,27 @@ MapPallete::MapPallete()
 				canvasPos.x + offsetX + offsetX * x + size * (float)x, 
 				canvasPos.y + offsetX + offsetX * y + size * (float)y
 			);
-			mButtonList.push_back(new ImageButton("Soil-01", worldPos, Vector2(size, size), Pivot::LeftTop,mCanvas->GetTransform()));
+			Slot slot;
+			string key = "Soil-";
+			if (index < 10)
+				key = key +  "0" + to_string(index);
+			else
+				key = key +  to_string(index);
+			slot.button = new ImageButton(key, worldPos, Vector2(size, size), Pivot::LeftTop, mCanvas->GetTransform());
+			mButtonList.push_back(slot);
+			++index;
 		}
 	}
-
-	mIsPick = false;
 }
 
 
 MapPallete::~MapPallete()
 {
+	for (UINT i = 0; i < mButtonList.size(); ++i)
+	{
+		SafeDelete(mButtonList[i].button);
+	}
+	mButtonList.clear();
 	SafeDelete(mCanvas);
 }
 
@@ -38,6 +50,13 @@ void MapPallete::Update()
 {
 	this->CanvasUpdate();
 
+	for (UINT i = 0; i < mButtonList.size(); ++i)
+	{
+		if (mButtonList[i].button->Update())
+		{
+
+		}
+	}
 
 }
 
@@ -46,7 +65,7 @@ void MapPallete::Render()
 	mCanvas->Render();
 
 	for (UINT i = 0; i < mButtonList.size(); ++i)
-		mButtonList[i]->Render();
+		mButtonList[i].button->Render();
 }
 
 const bool MapPallete::MouseOnPallete()
--- Spelunky/Spelunky/MapPallete.h
+++ Spelunky/Spelunky/MapPallete.h
@@ -1,13 +1,21 @@
 #pragma once
 class MapPallete final 
 {
+private:
+	struct Slot
+	{
+		class ImageButton* button;
+
+		Slot()
+			:button(nullptr) {}
+	};
 public:
 	MapPallete(const MapPallete& t) = delete;
 	const MapPallete& operator=(const MapPallete& t) = delete;
 private:
 	class Canvas* mCanvas;
 
-	vector<class ImageButton*> mButtonList;
+	vector<class Slot> mButtonList;
 
 	Vector2 mPickOffset; 
 	bool mIsPick; 
